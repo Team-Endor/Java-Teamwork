fab0be4fcc2394b6b883844e9ba9a5b6b44d83cd
DannyHr <dani_a1@ymail.com>
2015-09-16 07:31:55 +0300

    Intersects method moved to physics/CollisionDetector

---
+++ src/physics/CollisionDetector.java
@@ -0,0 +1,16 @@
+package physics;
+
+import java.awt.*;
+
+public class CollisionDetector {
+    public static boolean intersects(Rectangle firstBoundingBox, Rectangle secondBoundingBox) {    // check for collision
+        if (firstBoundingBox.x + firstBoundingBox.width >= secondBoundingBox.x &&
+                firstBoundingBox.y + firstBoundingBox.height >= secondBoundingBox.y &&
+                firstBoundingBox.y <= secondBoundingBox.y + secondBoundingBox.height &&
+                firstBoundingBox.x <= secondBoundingBox.x + secondBoundingBox.width) {
+            return true;
+        }
+
+        return false;
+    }
+}
